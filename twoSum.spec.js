298119126eef791eb60436a85bbe62f53164ffab
Bryan Gottschalk <bryanlgottschalk@gmail.com>
2019-06-27 11:41:41 -0500

    fixed twoSum array spec

--- twoSum.spec.js
+++ twoSum.spec.js
@@ -1,8 +1,10 @@
 const { expect } = require('chai');
 const twoSum = require('./twoSum');
 
+console.log(expect);
+
 describe('twoSum function', () => {
   it('returns an array', () => {
-    expect(twoSum([], 4).to.be.an('array'));
+    expect(twoSum([], 4)).to.be.an('array');
   });
 });
